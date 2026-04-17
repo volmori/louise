6f3b24b79a9122a6bd70efdfe81a5faf8d4ab478
dos <114744003+volmori@users.noreply.github.com>
2026-04-18 00:08:05 +0800

    Update configuration for title and messages

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
 const CONFIG = {
-    titleWeb: "louise",
-    introTitle: 'hello louise',
+    titleWeb: "Pau",
+    introTitle: 'hello Pau',
     introDesc: `I just wanted to tell you that you are the most amazing girl I know. You are perfect, and I can’t imagine a life without you.`,
     btnIntro: 'HI!',
     title: 'I’m in love with you. I don’t just like you or think you are hot. I am madly, deeply, indeed in love with you.',
     desc: 'do you love me? ',
     btnYes: 'yes',
     btnNo: 'no',
-    question: ' you louise ha pinindot mo yes',
+    question: ' you PauPau ha pinindot mo yes',
     btnReply: 'Sent to ry',
     reply: 'i love you ry',
-    mess: 'I know that . Love you so too',
-    messDesc: ' I still fall in love with you a little more every day. You are so wonderful, and I will never forget the times we shared. Every moment I am with you makes me love you more. You are like nothing I’ve ever experienced, and I can’t picture life without you',
+    mess: 'enebet . Love you so too',
+    messDesc: ' I really want to be the guy who treats you right and protects you from everything,ng walang kapalit  or lust man iyan, just pure love. I know I’m not perfect and I have my own problem mentally and physically, so I completely respect whatever your feelings are and I won’t pressure you at all. I just had to be honest about how much you mean to me, so yeah, mahal na mahal kita, Paupau."
+',
     btnAccept: 'i love you too ry',
    
 }
